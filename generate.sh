8f06a10254f35abc24a456c6551404dadd94b9bc
Iurii Mozol <yura.mozol@gmail.com>
2024-12-13 18:42:55 -0700

    R121.11: fixed generator

--- generate.sh
+++ generate.sh
@@ -5,6 +5,7 @@ echo '{"invokerPackage":"com.docstudio.client",
         "modelPackage":"com.docstudio.client.model",
         "groupId":"com.docstudio",
         "artifactId":"docstudio-api-client",
+        "disallowAdditionalPropertiesIfNotPresent": false,
         "artifactVersion": "1.0.0"}' > config.json
 npx openapi-generator-cli generate -i https://api.docstudio.com/v3/api-docs --skip-validate-spec -g java -o ./generated-client -c config.json
 cp -rf ./generated-client/src/main/ ./src
